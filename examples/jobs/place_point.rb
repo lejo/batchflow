6e759d17b1cbae27525cc3f587ba970a0d28f8b4
Lejo <email@lejovarughese.com>
2011-12-06 22:35:10 -0500

    white space correction

--- examples/jobs/place_point.rb
+++ examples/jobs/place_point.rb
@@ -1,5 +1,4 @@
 job :place_point do
-	triggered_by ':ticker_values || (:company_ingested && :company_district_mapped)'
-
-	trigger :ticker_values
+  triggered_by ':ticker_values || (:company_ingested && :company_district_mapped)'
+  trigger :ticker_values
 end
