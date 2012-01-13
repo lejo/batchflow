42c4a201628055e01af662d9696f07dba5b989f5
Lejo <email@lejovarughese.com>
2012-01-12 23:57:35 -0500

    renaming alert fixture to file ingestion

--- spec/fixtures/jobs/people_csv_ingestion.rb
+++ spec/fixtures/jobs/people_csv_ingestion.rb
@@ -1,6 +1,6 @@
-job "simple job" do |j|
+job "ingest people csv" do |j|
   j.task "wait for file" do |t|
-    t.triggered_by :type => :file, :path => "/path/to/file", :events => [:create]
+    t.triggered_by :type => :file, :path => "/path/to/people.csv", :events => [:create]
 #     # runs         :anytime
 #     # execute      Alert::Send
 #     # on_error     Notify::Failure
--- spec/lib/batchflow/dsl_spec.rb
+++ spec/lib/batchflow/dsl_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe BatchFlow::Dsl do
   context "the alert job" do
-    let('dsl') { BatchFlow::Dsl.new("spec/fixtures/jobs/alert.rb") }
+    let('dsl') { BatchFlow::Dsl.new("spec/fixtures/jobs/people_csv_ingestion.rb") }
     it { dsl.jobs.size.should == 1 }
-    it { dsl.jobs.first.name.should == "simple job" }
+    it { dsl.jobs.first.name.should == "ingest people csv" }
     context "tasks" do
       let('tasks') { dsl.jobs.first.tasks }
       it { tasks.size.should == 2 }
@@ -15,7 +15,7 @@ describe BatchFlow::Dsl do
           let('triggers') {first_task.triggers}
           it {triggers.size.should == 1}
           it {triggers.first.type.should == :file }
-          it {triggers.first.path.should == "/path/to/file" }
+          it {triggers.first.path.should == "/path/to/people.csv" }
           it {triggers.first.events.should == [:create]}
         end
       end
